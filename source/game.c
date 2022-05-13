daaaca7eee68f3b2174e5111afdc2b3fa5442e4b
ostef <47391841+ostef@users.noreply.github.com>
2022-05-13 13:36:36 +0200

    Raycasting first drafts.

--- source/game.c
+++ source/game.c
@@ -6,7 +6,7 @@
 /*   By: soumanso <soumanso@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 17:38:26 by soumanso          #+#    #+#             */
-/*   Updated: 2022/05/05 17:39:09 by soumanso         ###   ########lyon.fr   */
+/*   Updated: 2022/05/05 18:07:13 by soumanso         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,21 @@ void	update(t_game *game)
 {
 	game->params.mouse_coord.x = 0;
 	game->params.mouse_coord.y = 0;
+	if (game->params.inputs[KEY_D])
+		game->player_pos.x += 0.2f;
+	if (game->params.inputs[KEY_A])
+		game->player_pos.x -= 0.2f;
+	if (game->params.inputs[KEY_W])
+		game->player_pos.y -= 0.2f;
+	if (game->params.inputs[KEY_S])
+		game->player_pos.y += 0.2f;
+	printf ("%.3f %.3f\n", game->player_pos.x, game->player_pos.y);
 }
+
+void	raycast_all(t_game *game);
 
 void	render(t_game *game)
 {
-	clear_image (&game->frame, rgba (0, 0, 0, 255));
-	for (int y = 0; y < game->frame.height; y += 1)
-	{
-		for (int x = 0; x < game->frame.width; x += 1)
-		{
-			set_px (&game->frame, x, y, rgba (
-				(t_u8)(x / (t_f32)game->frame.width * 255),
-				(t_u8)(y / (t_f32)game->frame.height * 255),
-				0,
-				255
-			));
-		}
-	}
+	clear_image (&game->frame, rgba (30, 30, 45, 255));
+	raycast_all (game);
 }
