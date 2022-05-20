fca36821d6e296298b0770032f490e94059ff53b
ostef <47391841+ostef@users.noreply.github.com>
2022-05-20 15:02:11 +0200

    Added vec2f_len and vec2f_normalized functions.

--- source/math/vec2f/geom.c
+++ source/math/vec2f/geom.c
@@ -6,7 +6,7 @@
 /*   By: soumanso <soumanso@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 13:44:58 by soumanso          #+#    #+#             */
-/*   Updated: 2022/05/17 16:54:43 by soumanso         ###   ########lyon.fr   */
+/*   Updated: 2022/05/20 15:00:53 by soumanso         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,19 @@ t_vec2f	vec2f_perp_cw(t_vec2f v)
 t_vec2f	vec2f_perp_ccw(t_vec2f v)
 {
 	return (vec2f (-v.y, v.x));
+}
+
+t_f32	vec2f_len(t_vec2f v)
+{
+	return (sqrtf (v.x * v.x + v.y * v.y));
+}
+
+t_vec2f	vec2f_normalized(t_vec2f v)
+{
+	t_f32	len;
+
+	len = vec2f_len (v);
+	if (len == 0)
+		return (vec2f (0, 0));
+	return (vec2f (v.x / len, v.y / len));
 }
